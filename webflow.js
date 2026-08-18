798cb6926dad6f266d01641d3b24d658870bfaea
Stas <stas@stasiosdesign.com>
2026-08-18 16:33:48 +0100

    tesitn gagain

--- webflow.js
+++ webflow.js
@@ -28,9 +28,9 @@ gsap.defaults({ ease: "osmo", duration: durationDefault });
 
 
 
-// ---------------------------
+// ---------------------------------
 // FUNCTION REGISTRY
-// -----------------------------
+// --------------------------------
 
 function initOnceFunctions() {
   initLenis();
