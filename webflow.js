76d1947f109f757e03693f0e96978de6bdffa0d0
Stas <stas@stasiosdesign.com>
2026-08-18 12:32:05 +0100

    Update webflow.js

--- webflow.js
+++ webflow.js
@@ -74,23 +74,52 @@ function runPageOnceAnimation(next) {
   const tl = gsap.timeline();
 
   tl.call(() => {
-    resetPage(next)
+    resetPage(next);
   }, null, 0);
 
   return tl;
 }
 
 function runPageLeaveAnimation(current, next) {
+  const transitionWrap = document.querySelector("[data-transition-wrap]");
+  const transitionDark = transitionWrap.querySelector("[data-transition-dark]");
+
   const tl = gsap.timeline({
-    onComplete: () => { current.remove() }
-  });
+    onComplete: () => {
+      current.remove(); 
+    }
+  })
+  
+  CustomEase.create("parallax", "0.7, 0.05, 0.13, 1");
   
   if (reducedMotion) {
     // Immediate swap behavior if user prefers reduced motion
     return tl.set(current, { autoAlpha: 0 });
   }
-
-  tl.to(current, { autoAlpha: 0, duration: 0.4 });
+  
+  tl.set(transitionWrap, {
+    zIndex: 2
+  });
+  
+  tl.fromTo(transitionDark, {
+    autoAlpha: 0
+  },{
+    autoAlpha: 0.8,
+    duration: 1.2,
+    ease: "parallax"
+  }, 0);
+  
+  tl.fromTo(current,{
+    y: "0vh"
+  },{
+    y: "-25vh",
+    duration: 1.2,
+    ease: "parallax",
+  }, 0);
+  
+  tl.set(transitionDark, {
+    autoAlpha: 0,
+  });
 
   return tl;
 }
@@ -106,12 +135,19 @@ function runPageEnterAnimation(next){
     return new Promise(resolve => tl.call(resolve, null, "pageReady"));
   }
   
-  tl.add("startEnter", 0.6);
+  tl.add("startEnter", 0);
+  
+  tl.set(next, {
+    zIndex: 3
+  });
   
   tl.fromTo(next, {
-    autoAlpha: 0,
-  },{
-    autoAlpha: 1,
+    y: "100vh"
+  }, {
+    y: "0vh",
+    duration: 1.2,
+    clearProps: "all",
+    ease: "parallax"
   }, "startEnter");
 
   tl.add("pageReady");
